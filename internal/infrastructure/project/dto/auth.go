5b180ceeacdc94aae8f5475d56479496d592139d
Jairo Galvez <jairo.jggc@gmail.com>
2026-05-25 21:37:22 -0500

    Feature/image (#28)
    * feat: modos de ejecucion
    
    * fix: imagen y version en una sola linea
    
    ---------
    
    Co-authored-by: Jairo Galvez <jgalvez@baufest.com>

--- internal/infrastructure/project/dto/auth.go
+++
@@ -1,6 +0,0 @@
-package dto
-
-type AuthDTO struct {
-	Plugin string        `yaml:"plugin,omitempty"`
-	Params AuthParamsDTO `yaml:"params,omitempty"`
-}
--- internal/infrastructure/project/dto/runtime.go
+++ internal/infrastructure/project/dto/runtime.go
@@ -1,8 +1,7 @@
 package dto
 
 type RuntimeDTO struct {
-	Image     string        `yaml:"image"`
-	Tag       string        `yaml:"tag"`
-	Build     BuildDTO      `yaml:"build,omitempty"`
-	Run       RunDTO        `yaml:"run,omitempty"`
+	Image string   `yaml:"image,omitempty"`
+	Build BuildDTO `yaml:"build,omitempty"`
+	Run   RunDTO   `yaml:"run,omitempty"`
 }
